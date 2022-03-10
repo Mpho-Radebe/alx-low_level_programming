ec39c545d63d22a683b466b466604191f86ed2ce
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-10 22:38:21 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@ int main(void)
 	char* message = " and that piece of art is"
 		" useful\" - Dora Korpar, 2015-10-19\n";
 	
-	while (*(message++) != '\0')
-		putc(*message, stdout);
+	while (*message != '\0')
+		putc(*(message++), stdout);
 	
 	return (1);
 }
