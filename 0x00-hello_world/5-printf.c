f58f7c00146ab00544746892b5a8901bb76f0edf
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-10 11:02:42 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: always return 0
+ */
+int main(void)
+{
+    printf("with proper grammer, but the outcome is a piece of art,");
+    return (0);
+}
