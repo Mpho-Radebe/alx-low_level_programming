300b73790983bcb0534c65bdfc7545a83b887096
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-11 17:45:04 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
+/**
+ * main - prints the last time of random number
+ * Return: returns 0 for successful execution
+ */
+int main(void)
+{
+	int first, second, third;
+
+	for (first = '0'; first <= '7'; first++)
+	{
+		for (second = first + 1; second <= '8'; second++)
+		{
+			for (third = second + 1; third <= '9'; third++)
+			{
+				putchar(first);
+				putchar(second);
+				putchar(third);
+				if (first < '7' || second < '8' || third < '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}				
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
