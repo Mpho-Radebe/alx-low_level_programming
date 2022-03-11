0e32dcde2bbc2c6e28dd81f9e68f66bb5a1ddb98
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-11 17:12:05 +0200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
+/**
+ * main - prints the last time of random number
+ * Return: returns 0 for successful execution
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		if (c != 'q' && c != 'e')
+			putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
