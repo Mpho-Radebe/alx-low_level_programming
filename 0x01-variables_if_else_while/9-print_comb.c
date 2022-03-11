8cfbc2296160a4cef434bf54036e8539efa747e9
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-11 17:22:14 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
+/**
+ * main - prints the last time of random number
+ * Return: returns 0 for successful execution
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		if (c != '9'){
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('$');
+
+	return (0);
+}
