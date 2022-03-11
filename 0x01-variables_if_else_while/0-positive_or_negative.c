2e1c15553751d7b1f2e79468b273d7afdcc609df
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-11 16:45:46 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
+/**
+ * main - prints the last time of random number
+ * Return: returns 0 for successful execution
+ */
 int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+		printf("%d is negative", n);
+	else if (n > 0)
+		printf("%d is positive", n);
+	else
+		printf("%d is zero", n);
+
+	return (0);
 }
