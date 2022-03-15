df49ee390545f86b610e0a8841c626ffacac800b
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 15:05:17 +0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - prints sign of a number
+ * @n: a number
+ * Return: absolute value of an integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
