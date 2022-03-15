98ed7a7943868c5a6b07ff511f068aa0b12c32e4
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 16:54:59 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,32 +1,24 @@
-#include "main.h"
+#include "main.h"	
 
 /**
- * print_to_98 - prints all natural numbers from n to 98 
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: from n
  */
 void print_to_98(int n)
 {
-	int increment = 0;
+	int increment = 0, numberDigits;
 	int i, j, temp1, temp2;
-	int numberDigits;
-
+	increment = 1;
 	if (n > 98)
 		increment = -1;
-	else
-		increment = 1;
-
 	for (i = n; i != 98; i += increment)
 	{
+		numberDigits = 0;
 		if (i == 0)
 			numberDigits = 1;
-		else
-			numberDigits = 0;
-
+		temp1 = i;
 		if (i < 0)
 			temp1 = -i;
-		else
-			temp1 = i;
-		/* reversing the digits in a number */
 		temp2 = 0;
 		while (temp1 != 0)
 		{
@@ -34,17 +26,14 @@ void print_to_98(int n)
 			numberDigits++;
 			temp1 /= 10;
 		}
-
 		if (i < 0)
 			_putchar('-');
-		/* printing the reversed digits */
 		while (temp2 != 0)
 		{
 			numberDigits--;
 			_putchar('0' + temp2 % 10);
 			temp2 /= 10;
 		}
-
 		for (j = 0; j < numberDigits;j++)
 			_putchar('0');
 
