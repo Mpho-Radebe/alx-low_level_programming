3b1c65a571960c9208b88dbc897a3c793d4e5b60
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 14:56:11 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_sign - prints sign of a number
+ * @n: a number
+ * Return: returns 1, -1 or 0
+ */
+int _print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1)
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
