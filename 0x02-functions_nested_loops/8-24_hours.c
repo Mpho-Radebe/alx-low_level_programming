d89bc923c02d638d2cd172807a66e2a56134c4ea
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 15:40:20 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,34 @@
-#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * jack_bauer - prints something about time
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int min = 0;
+
+	while (hour < 24)
+	{
+		/* print hour */
+		_putchar('0' + hour / 10 % 10);
+		_putchar('0' + hour % 10);
+
+		_putchar(':');
+
+		/* print minute */
+		_putchar('0' + min / 10 % 10);
+		_putchar('0' + min % 10);
+
+		_putchar('\n');
+
+		min++;
+
+		if (min >= 60)
+		{
+			hour++;
+			min = 0;
+		}
+	}
+}
