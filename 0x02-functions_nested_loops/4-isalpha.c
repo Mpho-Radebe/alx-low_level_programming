7d0cfa850d79811021d68c5de2f6af70d35e6505
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 14:48:38 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is an alphabet
+ * @c: character
+ * Return: returns 1 if alphabet, and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
