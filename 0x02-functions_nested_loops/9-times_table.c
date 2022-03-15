496676fff9ba5193426090de6213d6befd95041e
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 16:00:37 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,39 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints something about time
+ */
+void times_table(void)
+{
+	int i, j;
+	int dig1, dig2, dig3;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		for (j = 1; j <= 9; j++)
+		{
+			dig1 = i * j / 100 % 10;
+			dig2 = i * j / 10 % 10;
+			dig3 = i * j % 10;
+
+			if (dig1 == 0)
+				_putchar(' ');
+			else
+				_putchar('0' + dig1);
+
+			if (dig2 == 0)
+				_putchar(' ');
+			else
+				_putchar('0' + dig2);
+
+			_putchar('0' + dig3);
+
+			if (j == 9)
+				_putchar('\n');
+			else
+				_putchar(',');
+		}
+	}
+}
