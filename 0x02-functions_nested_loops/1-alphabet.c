9a8f1086d9dedf92b1090ae3355c384a7454ab52
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 14:11:27 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,5 +3,8 @@
  * print_alphabet - prints alphabets 
  */
 void print_alphabet(void){
-	_putchar("abcdefghijklmnopqrstuvwxyz\n");
+	for (char ch = 'a'; ch <= 'z'; ch++) 
+		_putchar(*ch);
+	char newLine = '\n';
+	_putchar(newLine);
 }
