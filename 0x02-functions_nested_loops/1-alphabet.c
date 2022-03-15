3abb728446b31456f351fbc0035bb949e36b0e8b
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-03-15 13:53:40 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,9 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets 
+ */
+void print_alphabet(void){
+	for (char ch = 'a'; ch <= 'z'; ch++) 
+		_putchar(ch);
+	_putchar('\n')
+}
