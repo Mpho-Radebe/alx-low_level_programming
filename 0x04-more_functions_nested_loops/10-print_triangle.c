8793ba9b8df553fafc5e5e0619893e4e9947e06a
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-19 07:57:23 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,5 +6,16 @@
  */
 void print_triangle(int size)
 {
+	int i, j;
 
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+			_putchar(' ');
+		_putchar('#');
+		_putchar('\n');
+	}
 }
