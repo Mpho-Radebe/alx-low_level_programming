86c5782fb752810f0d77dfffeeb3a3f2e3531c40
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-19 02:58:40 +0200

    modified 1-isdiit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if a character is a digit
- * @c: a character
- * Return: returns 1 if c is a digit, and 0 otherwise
+ * _isdigit - checks if a character is a lowercase alphabet
+ * @c: character
+ * Return: returns 1 if lowercase, and 0 otherwise
  */
 int _isdigit(int c)
 {
