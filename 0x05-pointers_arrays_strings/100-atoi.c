578119d152225f8ea62d683390f802fe72282161
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-23 00:44:44 +0200

    the function atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,17 @@
  */
 int _atoi(char *s)
 {
-	return atoi(s);
+	int positive = 1;
+
+	while (*s == '\0' && !(*s >= '0' && *s <= '9'))
+	{
+		if (*s == '-')
+			positive = !positive;
+		s++;
+	}
+	
+	if (positive)
+		return (atoi(s));
+	else
+		return (-atoi(s));
 }
