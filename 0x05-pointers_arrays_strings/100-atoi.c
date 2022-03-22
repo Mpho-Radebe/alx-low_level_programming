54468e6d7bf161513362401608b91b8c90ff48c7
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-23 00:56:57 +0200

    the function atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 	int positive = 1;
 
 	if (strcmp("-2147483648", s) == 0)
-		return -2147483648;
+		return (-2147483648);
 
 	while (*s != '\0' && !(*s >= '0' && *s <= '9'))
 	{
