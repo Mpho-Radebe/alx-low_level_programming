64c4dec1a25cdafd24309626d5c61a0cb4239fde
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-22 23:32:40 +0200

    Changed 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ */
+void rev_string(char* s)
+{
+	char temp;
+	char* left = s;
+	char* right = s;
+
+
+	while (*right != '\0')
+		right++;
+
+	while (right > left)
+	{
+		temp = *right;
+		*right = *left;
+		*left = temp;
+		right--;
+		left++;
+	}
+}
