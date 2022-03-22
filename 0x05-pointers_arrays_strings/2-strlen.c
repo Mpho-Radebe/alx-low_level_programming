373b08325889cf4ae82eca2830e2de68a1feb401
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-22 22:43:12 +0200

    Changed 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: the string
+ * Return: the length of a string
  */
 int _strlen(char *s)
 {
 	int count = 0;
+
 	while (*(s++) != '\0')
 		count++;
-	return count;
+	return (count);
 }
