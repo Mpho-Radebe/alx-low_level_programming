7358342a66dc1c250a7f241ffc0087ba97c4f4c2
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-23 00:04:11 +0200

    Changed 7-puts_half.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - prints numbers in the array
+ * @a: the array
+ * @n: the number
+ */
 void print_array(int *a, int n)
 {
 	int i;
+
+	if (n <= 0)
+		putchar('\n');
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		
+
 		if (i < n - 1)
 			printf(", ");
 		else
 			putchar('\n');
 	}
+
 }
