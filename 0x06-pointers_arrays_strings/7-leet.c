667034b9d005e0007177c395c5b4e6c7eb6811f5
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-25 00:32:39 +0200

    Modified 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * leet - a function
+ * @str: string
+ * Return: string 
+ */
+char *leet(char *str)
+{
+	int i = 0;
+	char c;
+	int digit[] = {
+		4, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1, 0,
+		0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0
+	};
+
+	while ((c = tolower(str[i])) != '\0')
+	{
+		if (c == 'a' || c == 'e' || c == 'o' || c == 't' || 'l')
+			str[i] = '0' + digit[c - 'a'];
+		i++;
+	}
+	
+	return (str);
+}
