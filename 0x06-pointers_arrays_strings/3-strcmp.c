7f0fd833e7d3b50bd97fe1e140d60bb673794ef4
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-24 22:57:09 +0200

    Modified 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: a number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
