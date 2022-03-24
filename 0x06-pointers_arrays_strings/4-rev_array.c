dd9bafc9c8081c35908135e4f28c0f4849b92e50
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-24 23:18:19 +0200

    Modified 4-rev_array .c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: size of array
+ */
+void reverse_array(int *a, int n)
+{
+	int left = (n - 1) / 2;
+	int right = n / 2;
+
+	while (left >= 0 && right < n)
+	{
+		int temp = a[left];
+		a[left] = a[right];
+		a[right] = temp;
+
+		right++;
+		left--;
+	}
+}
