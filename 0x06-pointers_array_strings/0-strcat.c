388152011c0ceb978d16316484cab8ccd601dc16
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-24 22:33:43 +0200

    Modified 0-strcat.c

---
+++ 0x06-pointers_array_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates strings
+ * @dest: destination
+ * @src: source
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
