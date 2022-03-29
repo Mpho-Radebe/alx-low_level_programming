db251888773869fcf058f97460d9886c89d6e506
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-29 12:25:18 +0200

    Modified 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '0' && *s != c)
+	while (*s != '\0' && *s != c)
 		s++;
 
-	if (*s == '\0')
+	if (*s == '\0' && c != '\0')
 		return (NULL);
 	return (s);
 }
