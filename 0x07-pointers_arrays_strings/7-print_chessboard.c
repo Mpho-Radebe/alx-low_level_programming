e86a1b621fd2d073bb8ec2f4fdc61a91844fd719
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-29 12:40:40 +0200

    Modified 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,8 +7,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
-		printf("%s\n", a[i]);
+	{
+		for(j = 0; j < 8; j++)
+			putchar(a[i][j]);
+		putchar('\n');
+	}
 }
