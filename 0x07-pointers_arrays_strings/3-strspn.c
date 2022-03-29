57286c84cbe0906ebf3ecba9cf27555db9972848
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-29 12:13:08 +0200

    Modified 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,10 @@
+#include "main.h"
 #include <string.h>
 
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string 1
- * @accept: string 2;
+ * @accept: string 2
  * Return: a number
  */
 unsigned int _strspn(char *s, char *accept)
