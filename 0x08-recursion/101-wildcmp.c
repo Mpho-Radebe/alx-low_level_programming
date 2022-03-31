0c29f7daca33895a015a9f09e0938dcb40e39f39
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-03-31 13:10:41 +0200

    implemented wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,51 @@
+/**
+ * wildcmp - wild cards
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 Or 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	char *wildchar, *nowildchar;
+
+	if (*s1 == '*' || *s1 == *s2 || *s2 == '*')
+	{
+		if (*s1 == *s2 && *s1 == '*'){
+			s1++;
+			s2++;
+		}
+		else if (*s1 == '*' || *s2 == '*')
+		{
+			if (*s1 == '*')
+			{
+				wildchar = s1;
+				nowildchar = s2;
+			}
+			else
+			{
+				wildchar = s2;
+				nowildchar = s1;
+			}
+
+			if (*nowildchar == '\0')
+				return (1);
+
+			if (*(wildchar + 1) == *nowildchar)
+				wildchar++;
+			else
+				nowildchar++;
+
+			s1 = nowildchar;
+			s2 = wildchar;
+		}
+		else
+		{
+			s1++;
+			s2++;
+		}
+
+		return (wildcmp(s1, s2));
+	}
+	return (0);
+}
