7de7525b45f269cf2d7f6c11769efa9fae3cf84d
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-01 21:18:57 +0200

    Modified some files.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char* argv[])
+{
+	if (argv != NULL)
+		printf("%d\n", argc);
+	return (0);
+}
