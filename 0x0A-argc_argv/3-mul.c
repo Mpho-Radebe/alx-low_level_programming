5bd5f1525795fda243cf8c26ff1a8a69e1375ed7
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-01 22:04:16 +0200

    Made some few changes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - main function
+ * @argc: arguments count
+ * @argc: argument vector
+ * Return: success - 0, or failure - 1
+ */
 int main(int argc, char *argv[])
 {
 	int num1, num2;
@@ -13,7 +19,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	
+
 	printf("%d\n", num1 * num2);
 
 	return (0);
