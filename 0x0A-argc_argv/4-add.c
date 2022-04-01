2c29754adb69f18dbb6a5b296575c196a84cb192
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-01 22:15:51 +0200

    Made some few changes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,17 +13,17 @@ int main(int argc, char* argv[])
 	long unsigned int i, j;
 	int sum = 0;
 
-	if (argc == 0)
+	if (argc == 1)
 	{
 		puts("0");
 		return (0);
 	}
 
-	for (i = 0; i < (long unsigned int)argc; i++)
+	for (i = 1; i < (long unsigned int)argc; i++)
 	{
 		for (j = 0; j < strlen(argv[i]); j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 			{
 				puts("Error");
 				return (1);
