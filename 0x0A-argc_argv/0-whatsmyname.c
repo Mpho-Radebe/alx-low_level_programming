ea4d51e671d465ebe77e8c8776c15a7f5c2d5bb8
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-01 21:12:41 +0200

    Completed the 0-whatsmyname.c program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char* argv[])
+{
+	puts(argv[0]);
+	return (0);
+}
