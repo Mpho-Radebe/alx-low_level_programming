b62839177bc45c24a719a4ddb9caff6e5b2ccb5f
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-05 16:26:35 +0200

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,23 +11,26 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **grid = malloc(sizeof(int*) * height);
-	
+	int **grid = malloc(sizeof(int *) * height);
+
 	if (grid == NULL)
 	{
 		fprintf(stderr, "failed allocation");
 		return (NULL);
 	}
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			fprintf(stderr, "failed allocation");
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
