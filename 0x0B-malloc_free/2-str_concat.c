beaa2b7dda1241a43504d66ab943c48a3a306a6b
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-05 15:44:35 +0200

    Modified 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2){
+	char *concat_str;
+	unsigned int s1_size = 0;
+	unsigned int s2_size = 0;
+	int i;
+
+	if (s1 != NULL)
+		s1_size += strlen(s1);
+	if (s2 != NULL)
+		s2_size += strlen(s2);
+
+	concat_str = malloc(sizeof(char) * (s1_size + s2_size + 1));
+
+	if (concat_str == NULL)
+	{
+		fprintf(stderr, "failed memory allocation");
+		return NULL;
+	}
+
+	concat_str[s1_size + s2_size] = '\0';
+	for (i = 0; i < s1_size; i++)
+		concat_str[i] = s1[0];
+
+	for (i = 0; i < s2_size; i++)
+		concat_str[s1_size + i] = s2[i];
+
+	return concat_str;
+}
