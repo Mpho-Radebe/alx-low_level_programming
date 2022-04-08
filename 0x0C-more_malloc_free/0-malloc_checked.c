330cf8deb06485a27ead36c8d1ab07d04035bde8
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-08 16:31:58 +0200

    Added firls for 0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *memory = malloc(b);
+
+	if (memory == NULL)
+	{
+		fprintf(stderr, "Memory allocation failed");
+		exit(8);
+	}
+
+	return memory;
+}
---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,2 @@
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
