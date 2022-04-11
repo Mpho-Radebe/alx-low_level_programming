515b8151b6d5417a1d1609a51c9127dd624eea0d
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-11 23:58:47 +0200

    Added 0x0E-structures_typedef files

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a dog
+ * @d: the dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: ");
+	if (d->name == NULL)
+		puts("(nil)");
+	else
+		puts(d->name);
+	
+	printf("Age: %f\n", d->age);
+
+	printf("Owner: ");
+	if (d->owner == NULL)
+		puts("(nil)");
+	else
+		puts(d->owner);
+}
