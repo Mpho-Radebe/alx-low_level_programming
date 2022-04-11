3f18c2b905e3df97f0f17493b5f6a7b1e7aed0d4
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-12 00:41:49 +0200

    Added 0x0E-structures_typedef files

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,1 +1,6 @@
+#ifndef MACRO_INCLUDE_H
+#define MACRO_INCLUDE_H
+
 #define SIZE 1024
+
+#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,6 @@
+#ifndef THIS_HEADER_H
+#definE THIS_HEADER_H
+
 #define ABS(x) ((x) >= 0 ? (x) : (-x))
+
+#endif
