3677f96d74a0048a6576e186733be7a4d234df8c
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-12 00:22:47 +0200

    Added 0x0E-structures_typedef files

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - create a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner the dog
+ * Return: a dog created
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t* d = malloc(sizeof(struct dog));
+
+	if (d->name != name)
+	{
+		d->name = malloc(strlen(name) + 1);
+		strcpy(d->name, name);
+	}
+
+	d->age = age;
+
+	if (d->owner != owner)
+	{
+		d->owner = malloc(strlen(owner) + 1);
+		strcpy(d->owner, owner);
+	}
+
+	return (d);
+}
