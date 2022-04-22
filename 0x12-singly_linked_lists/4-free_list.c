55f4472ab2d81b5ef08700452c927aa7dfc84d79
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-22 23:04:18 +0200

    Modified 3-node_node_end.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees the list
+ * @head: head of the list
+ */
+void free_list(list_t *head)
+{
+	if(head == NULL)
+		return;
+	free_list(head->next);
+	free(head);
+}
