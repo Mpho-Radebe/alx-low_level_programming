c3b3fea0052cbb8baa14ed2d4dcfc9827a7eaf2e
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-28 20:26:43 +0200

    Fixed error

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,5 @@ int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 void _putchar (int ch);
-
+int get_endianness(void);
 #endif
