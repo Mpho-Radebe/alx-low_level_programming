5f3894744ac5b6267f253946ef0f932ae1daf193
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-28 19:35:42 +0200

    Fixed error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,12 +13,12 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int nextDigit = 1;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	for (index = strlen(b) - 1; index >= 0; index--)
 	{	
 		if (b[index] != '0' && b[index] != '1')
-			return 0;
+			return (0);
 
 		if (b[index] == '1')
 			result += nextDigit;
