7896e02239f633bea6a45a5e1a230df49ff891f8
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-04-28 19:49:53 +0200

    Fixed betty error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * recursive_print - prints the binary representation
+ * get_bit - prints the binary representation
  * @n: the number
+ * @index: the index
+ * Return: returns an integer
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index == 0)
-		return 0x01ul & n;
+		return (0x01ul & n);
 	else if (index > 0)
-		return get_bit(n >> 1, index - 1);
+		return (get_bit(n >> 1, index - 1));
 	else
-		return -1;
+		return (-1);
 }
