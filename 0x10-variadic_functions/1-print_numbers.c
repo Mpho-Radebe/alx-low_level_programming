c3589b885ff01603cec530f8873c568b951436dc
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-18 23:51:45 +0200

    Added files for the variadic functions project

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * @seperator: seperator
+ * @n: number
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	
+	if (n >= 1)
+		printf("%d", va_arg(ap, int));
+
+	for (i = 1; i < n; i++)
+	{
+		if (separator == NULL)
+			printf("%d", va_arg(ap, int));
+		else
+			printf("%s%d", separator, va_arg(ap, int));
+	}
+	printf("\n");
+}
