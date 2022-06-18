2a9025b4e9a629c2557ebaa6139434eae008aedd
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-18 23:21:01 +0200

    Added files for the variadic functions project

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sums the numbers
+ * @n: the number
+ * Return: integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+
+	return (sum);
+}
+
