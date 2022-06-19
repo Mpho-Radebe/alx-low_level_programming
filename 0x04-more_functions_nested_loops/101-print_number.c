1487e8cdaf2be2beddc38238ee21be8c26220c7b
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-20 01:11:43 +0200

    Fixing low level programmming 0x04 project

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+void print_num(int n);
+
+/**
+ * print_number - prints a number
+ * @n: the number
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{	_putchar('-');	
+		n = -n;
+	}
+	print_num(n);
+}
+
+/**
+ * print_num - prints number
+ * @n: the number
+ */
+void print_num(int n)
+{
+	if (n == 0)
+		return;
+	print_num(n / 10);
+	_putchar('0' + n % 10);
+}
+
