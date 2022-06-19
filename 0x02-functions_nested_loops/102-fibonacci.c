cc61d2acddd9e8beaa6fdccf3dc8b35f9a4d646c
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-19 22:51:16 +0200

    Fixing low level pro 0x02 project

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+	long prev;
+	long curr;
+	long temp;
+
+	printf("1");
+
+	prev = 1;
+	curr = 2;
+
+	for (int i = 1; i < 50; i++)
+	{
+		printf(", %ld", curr);
+		temp = curr;
+		curr = curr + prev;
+		prev = temp;
+	}
+	printf("\n");
+
+	return (0);
+}
+
