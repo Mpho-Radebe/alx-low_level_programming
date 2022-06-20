edae8a7d3a8fb388b897c86c52ab70bba9dab2fe
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-20 11:26:00 +0200

    Fixing 0x06 project of low level programming

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * count_digits - counts the number of digits in a number
+ * @n: the number
+ * Return: integer
+ */
+int count_digits(int n)
+{
+	int count = 0;
+
+	if (n == 0)
+		return (1);
+
+	while (n != 0)
+	{
+		count++;
+		n /= 10;
+	}
+	return (count);
+}
+
+/**
+ * infinite_add - adds
+ * @n1: number 1
+ * @n2: number 2
+ * @r: buffer
+ * @size_t: buffer size
+ * Return: string
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int sum = atoi(n1) + atoi(n2);
+
+	if (count_digits(sum) > size_r)
+		return (0);
+	sprintf(r, "%d", sum);
+	return (r);
+}
