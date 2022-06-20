18e742e2450fec7cf2ad47438e39dbcf289e462b
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-20 13:59:09 +0200

    Fixing 0x06 project of low level programming

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,6 +10,8 @@ void print_buffer(char *b, int size)
 {
 	int i, j;
 
+	if (size <= 0 || b == NULL)
+		putchar('\n');
 	i = 1;
 	while (i < size)
 	{
@@ -30,20 +32,16 @@ void print_buffer(char *b, int size)
 		if ((i - 1) % 10 == 0 || i >= size)
 		{
 			if (i >= size)
-			{
 				while ((i - 1) % 10 != 0)
 				{
 					printf("0000 ");
 					i += 2;
 				}
-			}
 			for (j = i - ((i - 2) % 10) - 1; j < i && j <= size; j++)
-			{
 				if (isprint(b[j - 1]))
 					putchar(b[j - 1]);
 				else
 					putchar('.');
-			}
 			putchar('\n');
 		}
 	}
