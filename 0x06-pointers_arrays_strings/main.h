62f25573d0f0e976b5d05ae671abddd65186e900
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-20 13:38:59 +0200

    Fixing 0x06 project of low level programming

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ char *rot13(char *);
 void print_number(int n);
 void _putchar(char ch);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 /**
  */
