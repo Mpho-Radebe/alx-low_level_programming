592b6af27e659215498813dede2dd0bbb8aaa69c
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-22 21:58:12 +0200

    Added correction for 0x12 project.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-void myStartupFun(void) __attribute___ ((constructor));
-
-void myStartupFun(void)
+void premain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
