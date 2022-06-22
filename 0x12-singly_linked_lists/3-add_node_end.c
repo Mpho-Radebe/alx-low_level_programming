29aa061dbd9b35658567582182509b2f31f28ebf
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-22 21:20:32 +0200

    Added correction for 0x12 project.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,26 +9,44 @@
  * Return: the last element
  */
 list_t *add_node_end(list_t **head, const char *str)
-{	
+{
 	unsigned int i;
 	list_t *endNode;
-	list_t *newNode = malloc(sizeof(list_t));
+	list_t *currNode;
+	list_t *tempNode;
+	list_t *newNode  = malloc(sizeof(list_t));
 
 	if (head == NULL)
 		return (NULL);
 
-	if (newNode == NULL)
-		return (NULL);
+	if (newNode != NULL)
+	{
+		newNode->len = strlen(str);
+		newNode->str = malloc(sizeof(char) * (newNode->len + 1));
+		newNode->next = NULL;
+	}
 
-	newNode->len = strlen(str);
-	newNode->str = malloc(sizeof(char) * (newNode->len + 1));
-	newNode->next = NULL;
+	if (newNode == NULL || newNode->str == NULL)
+	{
+		currNode = *head;
+		while (currNode != NULL)
+		{
+			tempNode = currNode;
+			currNode = currNode->next;
+			free(tempNode->str);
+			free(tempNode);
+		}
 
-	if (newNode->str == NULL)
+		if (newNode->str != NULL)
+			free(newNode->str);
+		if (newNode != NULL)
+			free(newNode);
 		return (NULL);
+	}
 
 	for (i = 0; i <= newNode->len; i++)
 		newNode->str[i] = str[i];
+	newNode->str[newNode->len] = '\0';
 
 	if (*head == NULL)
 	{
@@ -41,6 +59,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		endNode = endNode->next;
 
 	endNode->next = newNode;
-
 	return (newNode);
 }
