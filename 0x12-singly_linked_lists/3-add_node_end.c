b2b9e0fce265eba6cf37be7f1aef56aa18dc63be
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-22 21:28:47 +0200

    Added correction for 0x12 project.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,21 +11,16 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	unsigned int i;
-	list_t *endNode;
-	list_t *currNode;
-	list_t *tempNode;
-	list_t *newNode  = malloc(sizeof(list_t));
+	list_t *endNode, *currNode, *tempNode, *newNode = malloc(sizeof(list_t));
 
 	if (head == NULL)
 		return (NULL);
-
 	if (newNode != NULL)
 	{
 		newNode->len = strlen(str);
 		newNode->str = malloc(sizeof(char) * (newNode->len + 1));
 		newNode->next = NULL;
 	}
-
 	if (newNode == NULL || newNode->str == NULL)
 	{
 		currNode = *head;
@@ -36,28 +31,24 @@ list_t *add_node_end(list_t **head, const char *str)
 			free(tempNode->str);
 			free(tempNode);
 		}
-
 		if (newNode->str != NULL)
 			free(newNode->str);
 		if (newNode != NULL)
 			free(newNode);
 		return (NULL);
 	}
-
 	for (i = 0; i <= newNode->len; i++)
 		newNode->str[i] = str[i];
 	newNode->str[newNode->len] = '\0';
-
 	if (*head == NULL)
 	{
 		*head = newNode;
 		return (newNode);
 	}
-
 	endNode = *head;
 	while (endNode->next != NULL)
 		endNode = endNode->next;
-
 	endNode->next = newNode;
 	return (newNode);
 }
+
