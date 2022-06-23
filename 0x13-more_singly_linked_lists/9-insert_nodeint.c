832d0512326f39b031ab23723e281a2469347b92
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 15:43:59 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,3 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index_ - indexes a list
+ * @head: the list
+ * @index: index
+ * Return: the element
+ */
+listint_t *get_nodeint_at_index_(listint_t *head, unsigned int index)
+{
+	while (head != NULL)
+	{
+		if (index == 0)
+			return (head);
+		head = head->next;
+		index--;
+	}
+	return (NULL);
+}
+
 /**
  * insert_nodeint_at_index - inserts a node at given index
  * @head: the list
@@ -7,6 +28,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *before_node = NULL;
 
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		if (idx == 0)
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
+		else
+		{
+			before_node = get_nodeint_at_index_(*head, idx - 1);
+			if (before_node == NULL)
+			{
+				free(new_node);
+				new_node = NULL;
+			}
+			else
+			{
+				new_node->next = before_node->next;
+				before_node->next = new_node;
+			}
+		}
+	}
+
+	return (new_node);
 }
 
