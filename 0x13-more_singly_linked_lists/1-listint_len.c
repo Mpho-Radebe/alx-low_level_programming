6464644d6cb53f86f0fc01f148b66164bae16bca
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 13:50:38 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * listint_len - counts the number of elements in a list
  * @h: the list
@@ -5,5 +7,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
+	int count = 0;
 
+	while (h != 0)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
+
