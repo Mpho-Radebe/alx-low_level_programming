936f21d9959045b01529c2efd06ab8d240ac9134
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 13:54:22 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * add_nodeint - adds an element into a list
  * @head: the list
@@ -6,5 +8,18 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t* newNode = malloc(sizeof(listint_t));
 
+	if (newNode != NULL)
+	{
+		newNode->n = n;
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
+
