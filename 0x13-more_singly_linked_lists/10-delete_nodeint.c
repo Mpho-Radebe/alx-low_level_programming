2e534a1a03f4a4de16d0c2e810e00b692a161892
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 16:28:43 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,3 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index_ - indexes a list
+ * @head: the list
+ * @index: index
+ * Return: the element
+ */
+listint_t *get_nodeint_at_index_(listint_t *head, unsigned int index)
+{
+	while (head != NULL)
+	{
+		if (index == 0)
+			return (head);
+		head = head->next;
+		index--;
+	}
+	return (NULL);
+}
+
 /**
  * delete_nodeint_at_index - deletes node at the given index
  * @head: the list
@@ -6,6 +27,35 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *before_node = NULL;
+	listint_t *delete_node = NULL;
 
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		delete_node = *head;
+		*head = (*head)->next;
+	}
+	else
+	{
+		before_node = get_nodeint_at_index_(*head, index - 1);
+		if (before_node != NULL)
+		{
+			delete_node = before_node->next;
+			if (before_node->next != NULL)
+				before_node->next = before_node->next->next;
+		}
+	}
+
+	if (delete_node != NULL)
+	{
+		free(delete_node);
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
 }
-
