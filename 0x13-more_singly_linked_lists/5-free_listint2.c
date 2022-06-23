b1dd7e6cf71de6c621d4c0032e79e685c8dd6327
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 14:42:20 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,25 @@
 #include "lists.h"
 
 /**
+ * free_listint - deallocates memory for a list
+ * @head: list
+ */
+void free_listint_(listint_t *head)
+{
+	if (head != NULL)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
+
+/**
  * free_listint2 - deallocates memory used by a list
  * @head: the list
  */
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
+	free_listint_(*head);
 	*head = NULL;
 }
 
