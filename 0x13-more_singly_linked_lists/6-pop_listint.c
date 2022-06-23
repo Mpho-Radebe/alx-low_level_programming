096995bc36331486a0674ad8f61f2652a3df2efd
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 14:52:35 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,3 +1,6 @@
+#include "lists.h"
+#include <stdlib.h>
+
 /**
  * pop_listint - pops
  * @head: the list
@@ -5,5 +8,17 @@
  */
 int pop_listint(listint_t **head)
 {
+	int n;
+	listint_t *delete_node;
 
+	if (head != NULL && *head != NULL)
+	{
+		n = (*head)->n;
+		delete_node = *head;
+		*head = (*head)->next;
+		free(delete_node);
+		return(n);
+	}
+	return (0);
 }
+
