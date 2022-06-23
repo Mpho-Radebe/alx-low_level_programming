ca1bbc0337bcb08e931f81e75f21ff808f28eef1
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 14:07:30 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,3 +1,6 @@
+#include "lists.h"
+#include <stdlib.h>
+
 /**
  * add_nodeint_end - adds a new node at the end of a list
  * @head: the list
@@ -6,5 +9,29 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *tail = NULL;	
 
+	if (newNode != NULL)
+	{
+		newNode->n = n;
+		newNode->next = NULL;
+		
+		if (*head = NULL)
+			*head = newNode;
+		else
+		{
+			tail = *head;
+			while (tail->next != NULL)
+				tail = tail->next;
+			tail->next = newNode;
+		}
+
+		return (newNode);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
+
