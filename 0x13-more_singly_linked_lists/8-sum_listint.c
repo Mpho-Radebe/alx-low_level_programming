a87b3f711a9f3e845114691825637bef7345c321
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-23 15:02:17 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * sum_listint - sums the integers in the list
  * @head: the list
@@ -5,5 +7,14 @@
  */
 int sum_listint(listint_t *head)
 {
+	int sum = 0;
 
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
 }
+
