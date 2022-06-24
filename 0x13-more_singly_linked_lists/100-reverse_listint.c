49c31a5df47da1834127c56163a5b7fb72dcf0ac
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-24 08:12:52 +0200

    Fixing 0x13 project

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,3 +1,7 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
 /**
  * reverse_listint - reverses the a list
  * @head: the list
@@ -5,6 +9,30 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *left = *head;
+	listint_t *right = NULL;
+	listint_t *temp_ptr = NULL;
+	int temp;
+	
+	if (head == NULL || *head == NULL)
+		return (NULL);
 
+	while (right->next != NULL)
+		right = right->next;
+	while (right != left && right->next != left)
+	{
+		temp = right->n;
+		right->n = left->n;
+		left->n = temp;
+
+		left = left->next;
+
+		temp_ptr = *head;
+		while (temp_ptr->next != right)
+			temp_ptr = temp_ptr->next;
+		right = temp_tr;
+	}
+
+	return *head;
 }
 
