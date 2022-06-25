8aa114ceb10cb414630a2af2b40cf722815e662b
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-25 19:48:11 +0200

    Fixing 0x13 projcet

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * find_listint_loop - finds a loop
  * @head: the list
@@ -5,6 +7,21 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *cursor1 = head;
+	listint_t *cursor2 = head;
 
+	while (cursor2 != NULL)
+	{
+		cursor1 = head;
+		while (cursor1 != cursor2)
+		{
+			if (cursor1 == cursor2->next)
+				return (cursor1);
+			cursor1 = cursor1->next;
+		}
+		cursor2 = cursor2->next;
+	}
+
+	return (NULL);
 }
 
