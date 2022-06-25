2182a0a85f5a628c870a56969ce7a99d38ea56fa
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-25 20:59:48 +0200

    Fixing 0x13 projcet

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - adds a node to a list
+ * @head: a pointer to the head of the list
+ * @n: the new element
+ * Return: the new head
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - adds a node to a list
+ * @head: a pointer to the head of the list
+ * @n: the new element
+ * Return: the new head
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *d_node;
+	dlistint_t *cursor;
+
+	if (new != NULL)
+	{
+		new->n = n;
+		if (*head == NULL)
+			new->prev = new->next = NULL;
+		else
+		{
+			new->next = NULL;
+			cursor = *head;
+			while (cursor->next = NULL)
+				cursor = cursor->next;
+			cursor->next = new;
+			new->prev = cursor;
+		}
+		*head = new;
+	}
+	else
+	{
+		cursor = *head;
+		while (cursor != NULL)
+		{
+			d_node = cursor;
+			cursor = cursor->next;
+			free(d_node);
+		}
+		*head = NULL;
+	}
+
+	return (*head);
+}
