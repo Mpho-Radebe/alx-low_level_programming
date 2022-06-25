007469e38dac0b36ec9a8dafc1ad0184dad7b24e
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-25 15:53:11 +0200

    Fixing 0x13 projcet

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,3 +1,48 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add_nodeint_ - adds an element into a list
+ * @head: the list
+ * @n: the element
+ * Return: the new element
+ */
+listint_t *add_nodeint_(listint_t **head, const int n)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+
+	if (newNode != NULL)
+	{
+		newNode->n = n;
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
+/**
+ * find - lookds if an element is in the list
+ * @list: the list
+ * @e: search element
+ * Return: integer
+ */
+int find(listint_t* list, const int e)
+{
+	while (list != NULL)
+	{
+		if (list->n == e)
+			return (1);
+		list = list->next;
+	}
+	
+	return (0);
+}
+
 /**
  * print_listint_safe - prints a list
  * @head: the list
@@ -5,6 +50,21 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	int count = 0;
+	listint_t* traversed = NULL;
 
+	while (head != NULL)
+	{
+		if (find(traversed, (unsigned long)head))
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_nodeint_(&traversed, (unsigned long)head);
+		head = head->next;
+		count++;
+	}
+
+	return count;
 }
-
