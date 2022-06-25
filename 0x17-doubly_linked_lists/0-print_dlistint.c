9ea513709515d7bb58b4cc4ffcdd6e1762bfbff2
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-25 20:34:36 +0200

    Fixing 0x13 projcet

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints doubly linked list
+ * @h: head of the list
+ * Return: the size of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (count);
+}
+
