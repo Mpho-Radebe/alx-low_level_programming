50a0718cb8ce296ac7827532d148ecdf94066273
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-26 09:12:04 +0200

    Fixing 0x17 project

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get the node at given index
+ * @head: head of the list
+ * @index: the index
+ * Return: the node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i = 0;
+
+	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+			break;
+		head = head->next;
+	}
+
+	return (head);
+}
+
