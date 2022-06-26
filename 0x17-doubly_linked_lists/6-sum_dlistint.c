7e7d7ed8a4eb645828d62f5db80493b4d679dddf
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-26 09:16:58 +0200

    Fixing 0x17 project

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums up
+ * @head: head of the list
+ * Return: the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->n;
+	}
+
+	return (sum);
+}
+
