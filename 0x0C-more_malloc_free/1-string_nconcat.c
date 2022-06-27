2ad72f4b88e9b99d33201f267aa43f9ae3947002
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-27 16:10:41 +0200

    Fixing 0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,11 +22,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	
+
 	if (len1 == 0 && len2 == 0)
 		return (NULL);
-	
-	result = malloc(sizeof(char) * (len1 + len2));
+
+	result = malloc(sizeof(char) * (len1 + len2) + 1);
+	result[len1 + len2] = '\n';
 
 	if (result == NULL)
 		return (NULL);
@@ -37,6 +38,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = len1;  i < len1 + len2 && i < len1 + n; i++)
 		result[i] = s2[i - len1];
 
-	return result;
+	return (result);
 }
 
